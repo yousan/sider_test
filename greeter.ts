b46b202fef73ae5a57fa6857f08d64b6fa2e4727
Yousan_O <yousan@l2tp.org>
2019-03-01 23:10:57 +0900

    commit

--- greeter.ts
+++ greeter.ts
@@ -1,24 +1,23 @@
 //bad comment spacing
-class Student
+class Student {
+  fullName: string;
 
-
-
-
-{
-    fullName: string;
-
-    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
 }
 
 interface Person {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 }
 
 function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+  return "Hello, " + person.firstName + " " + person.lastName;
 }
 
 let user = new Student("Jane", "M.", "User");
